5f40b515e1f29e60082e7be7924ee95d31a67da3
Your name here <ebuzzadvert@gmail.com>
2020-01-12 17:42:07 +0100

    course page added

---
+++ iacademy/course-details.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <link rel="icon" href="img/logoicon.png" type="image/png" />
+    <title>Graphic Design Course Details :: iAcademy&trade;</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/flaticon.css" />
+    <link rel="stylesheet" href="css/themify-icons.css" />
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css" />
+    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css" />
+    <!-- main css -->
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <!--================ Start Header Menu Area =================-->
+    <header class="header_area white-header">
+      <div class="main_menu">
+        <div class="search_input" id="search_input_box">
+          <div class="container">
+          </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <a class="navbar-brand" href="\">
+              <img class="logo-2" src="img/iacademy_logo.png" alt="" />
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="icon-bar"></span> <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div
+              class="collapse navbar-collapse offset"
+              id="navbarSupportedContent"
+            >
+              <ul class="nav navbar-nav menu_nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about-us.html">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link primary-btn3" href="contact.html">Get Started</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!--================ End Header Menu Area =================-->
+
+    <!--================Home Banner Area =================-->
+    <section class="banner_area">
+      <div class="banner_inner d-flex align-items-center">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-6">
+              <div class="banner_content text-center">
+                <h2>Course Details</h2>
+                <!--
+                <div class="page_link">
+                  <a href="index">Home</a>
+                  <a href="courses.html">Courses</a>
+                  <a href="course-details.html">Courses Details</a>
+                </div>
+-->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================End Home Banner Area =================-->
+
+    <!--================ Start Course Details Area =================-->
+    <section class="course_details_area section_gap">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 course_details_left">
+                    <div class="main_image">
+                        <img class="img-fluid" src="img/courses/course-details.jpg" alt="">
+                    </div>
+                    <div class="content_wrapper">
+                        <h4 class="title">Objectives</h4>
+                        <div class="content">
+                            When you enter into any new area of science, you almost always find yourself with a
+                            baffling new language of
+                            technical terms to learn before you can converse with the experts. This is certainly
+                            true in astronomy both in
+                            terms of terms that refer to the cosmos and terms that describe the tools of the trade,
+                            the most prevalent
+                            being the telescope.
+                            <br>
+                            <br>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore
+                            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                            ut aliquip ex ea
+                            commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum. Lorem ipsum dolor sit
+                            amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim
+                            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat. Duis aute
+                            irure dolor in reprehenderit in voluptate velit esse cillum.
+                        </div>
+
+                        <h4 class="title">Eligibility</h4>
+                        <div class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore
+                            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                            ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
+                            <br>
+                            <br>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore
+                            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                            ut aliquip ex ea
+                            commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum. Lorem ipsum dolor sit
+                            amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim
+                            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat. Duis aute
+                            irure dolor in reprehenderit in voluptate velit esse cillum.
+                        </div>
+
+                        <h4 class="title">Course Outline</h4>
+                        <div class="content">
+                            <ul class="course_list">
+                                <li class="justify-content-between d-flex">
+                                    <p>Introduction Lesson</p>
+                                    <a class="primary-btn text-uppercase" href="#">View Details</a>
+                                </li>
+                                <li class="justify-content-between d-flex">
+                                    <p>Basics of HTML</p>
+                                    <a class="primary-btn text-uppercase" href="#">View Details</a>
+                                </li>
+                                <li class="justify-content-between d-flex">
+                                    <p>Getting Know about HTML</p>
+                                    <a class="primary-btn text-uppercase" href="#">View Details</a>
+                                </li>
+                                <li class="justify-content-between d-flex">
+                                    <p>Tags and Attributes</p>
+                                    <a class="primary-btn text-uppercase" href="#">View Details</a>
+                                </li>
+                                <li class="justify-content-between d-flex">
+                                    <p>Basics of CSS</p>
+                                    <a class="primary-btn text-uppercase" href="#">View Details</a>
+                                </li>
+                                <li class="justify-content-between d-flex">
+                                    <p>Getting Familiar with CSS</p>
+                                    <a class="primary-btn text-uppercase" href="#">View Details</a>
+                                </li>
+                                <li class="justify-content-between d-flex">
+                                    <p>Introduction to Bootstrap</p>
+                                    <a class="primary-btn text-uppercase" href="#">View Details</a>
+                                </li>
+                                <li class="justify-content-between d-flex">
+                                    <p>Responsive Design</p>
+                                    <a class="primary-btn text-uppercase" href="#">View Details</a>
+                                </li>
+                                <li class="justify-content-between d-flex">
+                                    <p>Canvas in HTML 5</p>
+                                    <a class="primary-btn text-uppercase" href="#">View Details</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-4 right-contents">
+                    <ul>
+                        <li>
+                            <a class="justify-content-between d-flex" href="#">
+                                <p>Trainer’s Name</p>
+                                <span class="or">George Mathews</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="justify-content-between d-flex" href="#">
+                                <p>Course Fee </p>
+                                <span>$230</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="justify-content-between d-flex" href="#">
+                                <p>Available Seats </p>
+                                <span>15</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="justify-content-between d-flex" href="#">
+                                <p>Schedule </p>
+                                <span>2.00 pm to 4.00 pm</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <a href="#" class="primary-btn2 text-uppercase enroll rounded-0 text-white">Enroll the course</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================ End Course Details Area =================-->
+<?php require_once'tawkto.php'; ?>
+    <!--================ Start footer Area  =================-->
+    
+    <footer class="footer-area footer-area2 section_gap">
+            <div class="container">
+              <div class="row footer-bottom justify-content-center">
+                <p class="footer-text m-0 text-white">
+&copy; Copyright <script>document.write(new Date().getFullYear());</script> All Rights Reserved  by <a href="http://ibrandafricagroup.one" target="_blank">iBrand Africa Group</a>
+                </p>
+                <!--
+                <div class="col-lg-4 col-sm-12 footer-social">
+                  <a href="#"><i class="ti-facebook"></i></a>
+                  <a href="#"><i class="ti-twitter"></i></a>
+                  <a href="#"><i class="ti-dribbble"></i></a>
+                  <a href="#"><i class="ti-linkedin"></i></a>
+                </div>
+                -->
+              </div>
+            </div>
+          </footer>
+  
+          <!--================ End footer Area  =================-->
+      
+          <!-- Optional JavaScript -->
+          <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+          <script src="js/jquery-3.2.1.min.js"></script>
+          <script src="js/popper.js"></script>
+          <script src="js/bootstrap.min.js"></script>
+          <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+          <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+          <script src="js/owl-carousel-thumb.min.js"></script>
+          <script src="js/jquery.ajaxchimp.min.js"></script>
+          <script src="js/mail-script.js"></script>
+          <!--gmaps Js-->
+          <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+          <script src="js/gmaps.min.js"></script>
+          <script src="js/theme.js"></script>
+        </body>
+      </html>
